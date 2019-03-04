9a32df66380b029342c7e2b6c36ea74a60df4862
Michal Starski <michalstarski99@gmail.com>
2019-03-05 00:19:21 +0100

    created classes inside modules instead of self methods

--- client_utilities.rb
+++ client_utilities.rb
@@ -1,31 +1,33 @@
-module Client
-    def self.connect_as_client
-        puts 'Please provide a valid domain'
-        domain = gets.to_s
-        puts 'Please provide a valid port'
-        port = gets.to_i
-        socket = TCPSocket.new(domain.chomp, port)
-        t1 = Thread.new{client_recv_message(socket)}
-        t2 = Thread.new{client_send_message(socket)}
-    
-        t1.join
-        t2.join
-    
-        socket.close
-    end
-    
-    def self.client_recv_message(socket_var)
-        puts socket_var.recv(255)
-    end
-    
-    def self.client_send_message(socket_var)
-        msg = '' 
-        loop {
-            print '# '
-            msg = gets.to_s
-            break if msg == "exit\n"
-            socket_var.write(msg)
-        }
-        socket_var.close
+module ClientModule
+    class Client
+        def connect_as_client
+            puts 'Please provide a valid domain'
+            domain = gets.to_s
+            puts 'Please provide a valid port'
+            port = gets.to_i
+            socket = TCPSocket.new(domain.chomp, port)
+            t1 = Thread.new{client_recv_message(socket)}
+            t2 = Thread.new{client_send_message(socket)}
+        
+            t1.join
+            t2.join
+        
+            socket.close
+        end
+        
+        def client_recv_message(socket_var)
+            puts socket_var.recv(255)
+        end
+        
+        def client_send_message(socket_var)
+            msg = '' 
+            loop {
+                print "# "
+                msg = gets.to_s
+                break if msg == "exit\n"
+                socket_var.write(msg)
+            }
+            socket_var.close
+        end
     end
 end
--- server_utilities.rb
+++ server_utilities.rb
@@ -1,29 +1,31 @@
-module Server
-    def self.create_server
-        puts 'Please provide a valid domain'
-        domain = gets.to_s
-        puts 'Please provide a valid port'
-        port = gets.to_i
-        puts 'Starting a server ...'
-        server = TCPServer.open(domain.chomp, port)
-        loop {
-            client = server.accept
-            t1 = Thread.new{handle_client_connection(client)}
-            t2 = Thread.new{recv_from_client(client)}
-        }
-        server.close
-    end
+module ServerModule
+    class Server
+        def create_server
+            puts 'Please provide a valid domain'
+            domain = gets.to_s
+            puts 'Please provide a valid port'
+            port = gets.to_i
+            puts 'Starting a server ...'
+            server = TCPServer.open(domain.chomp, port)
+            loop {
+                client = server.accept
+                t1 = Thread.new{handle_client_connection(client)}
+                t2 = Thread.new{recv_from_client(client)}
+            }
+            server.close
+        end
 
-    def self.handle_client_connection(client)
-    sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
-        puts "New user connected: #{remote_hostname}:#{remote_port}"
-        client.write("Connection successful\n To exit simply type 'exit'")
-    end
-
-    def self.recv_from_client(client) 
+        def handle_client_connection(client)
         sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
-        loop {
-            puts "#{remote_ip}:#{remote_port} [#{Time.now}]: #{client.recv(255)}"
-        }
+            puts "New user connected: #{remote_hostname}:#{remote_port}"
+            client.write("Connection successful\n To exit simply type 'exit'")
+        end
+
+        def recv_from_client(client) 
+            sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
+            loop {
+                puts "#{remote_ip}:#{remote_port} [#{Time.now}]: #{client.recv(255)}"
+            }
+        end
     end
 end
